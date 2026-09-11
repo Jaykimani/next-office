1372b1e7bfd66626913cccee9929785cd63e143f
Jaykimani <josephkimani1998@gmail.com>
2026-09-11 16:17:16 +0300

    vercel commit 31

--- access/adminAccess.ts
+++ access/adminAccess.ts
@@ -1,5 +1,11 @@
 import { Access } from "payload"
 
 export const adminAccess: Access = ({ req: { user } }) => {
-  return user?.role === "admin" || user?.role === "manager"
+  if (!user) return false
+
+  if (user.collection !== 'users') {
+    return false
+  }
+
+  return user.role === 'admin' || user.role === 'manager'
 }
